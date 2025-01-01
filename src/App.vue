a3d5ea6ad4c7501e06c9ef5a8834b6c27f5ab578
away-star <2064989403@qq.com>
2025-01-01 15:39:50 +0800

    feat: vercel尝试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
-<template>
-    <Example></Example>
-</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue';
 
-<script setup lang="ts">
-import Example from './example/Example.vue'
+onMounted(() => {
+    // 动态添加第一个脚本
+    const script1 = document.createElement('script');
+    script1.src = 'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.1.0-beta.0/libs/cn/index.js';
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    // 等待第一个脚本加载完成后再添加第二个脚本
+    script1.onload = () => {
+        const script2 = document.createElement('script');
+        script2.innerHTML = `
+new CozeWebSDK.WebChatClient({
+  config: {
+    botId: '7355054488501764096',
+  },
+  userInfo: {
+      id: '123',
+      url: 'https://lf-coze-web-cdn.coze.cn/obj/coze-web-cn/obric/coze/favicon.1970.png',
+      nickname: '3qweqweqwe',
+    },
+    ui: {
+      base: {
+          layout: 'mobile',
+      }
+    },
+});
+
+    `;
+        document.body.appendChild(script2);
+    };
+});
 </script>
 
-<style lang="scss"></style>
+<template>
+    <div>
+        <!-- 你的组件内容 -->
+    </div>
+</template>
